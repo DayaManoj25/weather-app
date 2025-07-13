845abe490322a2f1bf3ae07101e2a2c7b610b6ff
DayaManoj25 <dayamanoj2005@gmail.com>
2025-07-13 23:20:20 +0530

    change

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
     BASE_URL: "https://api.openweathermap.org/data/2.5", //base url of the api
-    GEO: "http://api.openweathermap.org/geo/1.0", //base url for geocoding
+    GEO: "https://api.openweathermap.org/geo/1.0", //base url for geocoding
     API_KEY: import.meta.env.VITE_OPENWEATHER_API_KEY, //fetches your API key from your environment file
     DEFAULT_PARAMS: { //Default query parameters you will use with every API call.
         units: "metric",
